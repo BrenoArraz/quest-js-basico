4fcbd3cce3cb537bc90a5e5ed050e97d14bcd115
Breno Arraz <brenogomesarraz@gmail.com>
2022-05-19 13:17:13 -0300

    quest js basico

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// -------  SOMA  ---------
+function somar(num1, num2){
+  let soma = num1 + num2
+  return 'A soma dos dois numeros é: ' + soma
+
+}
+let resultadoSoma = somar(5, 10)
+console.log(resultadoSoma)
+
+// --------  SUBTRAÇÃO  -------
+function subtrair(num1, num2){
+  let subtracao = num1 - num2
+  return 'A subtração dos dois numeros é: ' + subtracao
+}
+let resultadoSubtracao = subtrair(5, 10)
+console.log(resultadoSubtracao)
+
+// -------  MULTIPLICAÇÃO  -------
+function multiplicar(num1, num2){
+  let multiplicacao = num1 * num2
+  return 'A multiplicação dos dois numeros é: ' + multiplicacao
+}
+let resultadoMultiplicacao = multiplicar(5, 10)
+console.log(resultadoMultiplicacao)
+
+// ------ DIVISÃO  ----------
+function dividir(num1, num2){
+  let divisao = num1 / num2
+  return 'A divisão dos dois numeros é: ' + divisao
+}
+let resultadoDivisao = dividir(5, 10)
+console.log(resultadoDivisao)
